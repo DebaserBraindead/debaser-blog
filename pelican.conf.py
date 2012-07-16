c72826c359c916e46fdc498a5458bbf9db289965
debaser <debaser@platypus.home>
2012-07-16 23:23:52 +0200

    first article & configuration

---
+++ pelican.conf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Debaser"
+SITENAME = u"Debaser's Blog"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG='fr'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+#    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('twitter', 'https://twitter.com/_debaser_'),
+          ('github', 'https://github.com/DebaserBraindead/'),
+         )
+
+DEFAULT_METADATA = (('debaser', 'braindead',),)
+
+DEFAULT_PAGINATION = 7
---
+++ src/premier_article.rst
@@ -0,0 +1,18 @@
+My super title
+##############
+
+:date: 2012-07-16 22:26
+:tags: first, post
+:category: misc
+:author: Debaser
+:slug: my-super-title
+:lang: fr
+
+N'ayant pas blogué depuis longtemps, j'expérimente pelican_.
+
+
+Aux premier abord très accessible pour quiconque maitrise les bases de la `gestion de version <http://git-scm.com/>`_,
+python_ et le `shell <http://www.zsh.org/>`_. En conclusion l'outils parfait pour moi :) 
+
+.. _pelican: http://pelican.readthedocs.org/en/2.8/index.html
+.. _python: http://python.org/
